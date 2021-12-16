96a547093b198eaca0e3b1191fc2868ea14f9b21
Charles Aggasid <charles.aggasid01@gmail.com>
2021-12-15 21:09:32 -0600

    console exercise java done

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -20,16 +20,16 @@ import java.util.Scanner;
 //What happens if you enter less than 3 words?  Using next, will proceed to next until all string is asked.
 //What happens if you enter more than 3 words? nothing
 
-             Scanner myObj = new Scanner(System.in);
-             System.out.println("Enter three words:");
-
-             String firstWord = myObj.next();
-             String secondWord = myObj.next();
-             String thirdWord = myObj.next();
-
-             System.out.printf("First word is: %s%n", firstWord);
-             System.out.printf("Second word is: %s%n", secondWord);
-             System.out.printf("Third word is: %s", thirdWord);
+//             Scanner myObj = new Scanner(System.in);
+//             System.out.println("Enter three words:");
+//
+//             String firstWord = myObj.next();
+//             String secondWord = myObj.next();
+//             String thirdWord = myObj.next();
+//
+//             System.out.printf("First word is: %s%n", firstWord);
+//             System.out.printf("Second word is: %s%n", secondWord);
+//             System.out.printf("Third word is: %s", thirdWord);
 
 
 //Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.
@@ -41,6 +41,39 @@ import java.util.Scanner;
 //              String sentence = mySentenceObj.nextLine();
 //
 //              System.out.printf("The sentence you wrote is: %s %n", sentence);
+
+/**Calculate the perimeter and area of Codeup's classrooms.**/
+
+//         1. Prompt the user to enter values of length and width of a classroom at Codeup (or your room if virtual).
+//
+//            Use the nextLine method each time you need to get user input. In this case, we need it twice, once to
+//            get the user input for the length and again to get the user input for the width. PARSE the resulting
+//            strings to a numeric type.
+//          * Assume that the rooms are perfect rectangles.
+//          * Assume that the user will enter valid numeric data for length and width.
+
+//         2. Display the area and perimeter of that classroom.
+//
+//            The area of a rectangle is equal to the length times the width, and the perimeter of a rectangle
+//            is equal to 2 times the length plus 2 times the width.
+float length, width, perimeter, area;
+Scanner scanPerimeter = new Scanner(System.in);
+
+//Input length
+            System.out.println("Enter length of rectangle: ");
+            length = scanPerimeter.nextFloat();
+
+            System.out.println("Enter width of rectangle: ");
+            width = scanPerimeter.nextFloat();
+
+            //Calculate perimeter of rectangle
+            perimeter = 2 * (length + width);
+
+            area = length * width;
+
+            System.out.println("Perimeter of rectangle is " + perimeter + ".");
+            System.out.println("Area of rectangle is " + area + ".");
+
         }
     }
 
