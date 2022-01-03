0bec568cb077c953cca8ccf0e2ec053315b047eb
Charles Aggasid <charles.aggasid01@gmail.com>
2022-01-03 11:41:01 -0600

    String exercises

---
+++ src/Bob.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Say something to Bob.");
+        String userInput = scanner.nextLine();
+
+        if(userInput.endsWith("?")) {
+            System.out.println("Sure");
+        } else if(userInput.endsWith("!")){
+            System.out.println("Whoa, chill out!");
+        } else if(userInput.equals("")){
+            System.out.println("Fine. Be that way!");
+        } else{
+            System.out.println("Whatever");
+        }
+
+    }
+}
---
+++ src/StringExercise.java
@@ -0,0 +1,16 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String StringOutput = "We don't need no education\nWe don't need no thought control";
+        System.out.println(StringOutput);
+
+        StringOutput = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(StringOutput);
+
+        StringOutput = "In windows, the main drive is usually C:\\";
+        System.out.println(StringOutput);
+
+        StringOutput = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(StringOutput);
+
+    }
+}
