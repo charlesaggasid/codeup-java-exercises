b60279506c101ef3f0467da7c1be33db4ad1a062
Charles Aggasid <charles.aggasid01@gmail.com>
2022-01-04 09:07:17 -0600

    updates

---
+++ src/MethodsLecture.java
@@ -0,0 +1,11 @@
+public class MethodsLecture {
+    public static void main(String[] args) {
+        System.out.println(returnSeven(7));
+    }
+
+    public static int returnSeven(int num){
+        return num;
+    }
+
+
+}
