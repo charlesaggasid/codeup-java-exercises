fcc8679156d0279cf038e7b2a673619771be9b7c
Charles Aggasid <charles.aggasid01@gmail.com>
2022-01-04 10:55:29 -0600

    methods exercise done

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,13 +1,16 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 import java.util.Random;
 
 
 public class MethodsExercises {
     public static void main(String[] args) {
-        System.out.println(Add(2,2));
-        System.out.println(Divide(2,5));
-//     getInteger(1,10);
-//        diceRoll();
+//        System.out.println(Add(2,2));
+//        System.out.println(Divide(2,5));
+//      getInteger(1,10);
+//      diceRoll();
+      factorial();
 
     }
     //======================================== Question 1 ==============================================================
@@ -29,14 +32,17 @@ public class MethodsExercises {
     public static int Add(int firstNum, int secondNum) {
         return firstNum + secondNum;
     }
+
     //Subs
     public static int Subs(int firstNum, int secondNum) {
         return firstNum - secondNum;
     }
+
     //Multiply
     public static int Multiply(int firstNum, int secondNum) {
         return firstNum * secondNum;
     }
+
     //Divide
     public static int Divide(int firstNum, int secondNum) {
         return firstNum / secondNum;
@@ -60,16 +66,16 @@ public class MethodsExercises {
 //    If the input is invalid, prompt the user again.
 //    HINT: recursion might be helpful.
 
-      public static int getInteger(int min, int max){
-          Scanner scanner = new Scanner(System.in);
-          System.out.println("Enter a number between 1 and 10: ");
-          int userInput = scanner.nextInt();
-          if(userInput >= min && userInput <= max){
-              return userInput;
-          }
-          System.out.println("Number not in between 1-10.");
-          return getInteger(min, max);
-      }
+    public static int getInteger(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number between 1 and 10: ");
+        int userInput = scanner.nextInt();
+        if (userInput >= min && userInput <= max) {
+            return userInput;
+        }
+        System.out.println("Number not in between 1-10.");
+        return getInteger(min, max);
+    }
     //============================================== Question 3 ========================================================
 //    - Prompt the user to enter an integer from 1 to 10.
 //    - Display the factorial of the number entered by the user.
@@ -85,6 +91,28 @@ public class MethodsExercises {
 //            2! = 1 x 2           = 2
 //            3! = 1 x 2 x 3       = 6
 //            4! = 1 x 2 x 3 x 4   = 24
+
+//        public static long factorial(int num){
+//            long n = 1;
+//            for (int i = 1; i <= n; i++){
+//                n = n * i;
+//            }
+//            return n;
+//        }
+
+    public static void factorial() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number between 1 and 10");
+        int num = scanner.nextInt();
+
+        long factorial = 9;
+        for(int i = 1; i <= num; ++i){
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+}
+
 
 
 
